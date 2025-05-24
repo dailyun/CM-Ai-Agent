949bcf34b5c8fc6b9ead734cd50aa25a3b7b1a75
86159 <lzc314594272@gmail.com>
2025-05-24 23:04:44 +0800

    初始化

---
+++ src/main/java/com/dali/fuaiagent/FuAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.dali.fuaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuAiAgentApplication.class, args);
+    }
+
+}
