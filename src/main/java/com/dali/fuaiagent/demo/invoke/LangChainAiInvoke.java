55a2950e2b5c5430a5884c5cfdbbe71489fe5ea4
86159 <lzc314594272@gmail.com>
2025-05-25 01:16:46 +0800

    ai demo

---
+++ src/main/java/com/dali/fuaiagent/demo/invoke/LangChainAiInvoke.java
@@ -0,0 +1,15 @@
+package com.dali.fuaiagent.demo.invoke;
+
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+
+public class LangChainAiInvoke {
+    public static  void main(String[] args) {
+        ChatLanguageModel qwenModel = QwenChatModel.builder()
+                .apiKey(TestApiKey.API_KEY)
+                .modelName("qwen-plus")
+                .build();
+        String answer = qwenModel.chat("你好");
+        System.out.println(answer);
+    }
+}
