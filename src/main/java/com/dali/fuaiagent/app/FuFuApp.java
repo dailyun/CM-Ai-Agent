bf7654fd46c00be88f438aaa9447d011a1bdaf17
86159 <lzc314594272@gmail.com>
2025-05-26 19:54:25 +0800

    add ToolRegistration

--- src/main/java/com/dali/fuaiagent/app/FuFuApp.java
+++ src/main/java/com/dali/fuaiagent/app/FuFuApp.java
@@ -14,6 +14,7 @@ import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
 
@@ -76,6 +77,23 @@ public class FuFuApp {
                 .chatResponse();
         String content = chatResponse.getResult().getOutput().getText();
 
+        return content;
+    }
+
+    @Resource
+    private ToolCallback[] allTools;
+    public String doChatWithTools(String messsage, String chatId) {
+        ChatResponse response = chatClient
+                .prompt()
+                .user(messsage)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .advisors(new MyLoggerAdvisor())
+                .tools(allTools)
+                .call()
+                .chatResponse();
+        String content = response.getResult().getOutput().getText();
+        log.info("content: {}", content);
         return content;
     }
 
--- src/test/java/com/dali/fuaiagent/app/FuFuAppTest.java
+++ src/test/java/com/dali/fuaiagent/app/FuFuAppTest.java
@@ -1,6 +1,7 @@
 package com.dali.fuaiagent.app;
 
 import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -24,4 +25,32 @@ class FuFuAppTest {
         String response = fuFuApp.doChatWithRag(message, chatId);
 
     }
+
+    @Test
+    void doChatWithTools() {
+        // 测试联网搜索问题的答案
+        testMessage("周末想带你去上海约会，推荐几个适合情侣的小众打卡地？");
+
+        // 测试网页抓取：恋爱案例分析
+        testMessage("你能帮我看看网站（pic.furinabox.me）有什么内容吗？");
+
+        // 测试资源下载：图片下载
+        testMessage("直接下载一张图片从（https://furinabox.me/furina.jpg）");
+
+        // 测试终端操作：执行代码
+        testMessage("执行 Python3 脚本来生成数据分析报告");
+
+        // 测试文件操作：保存用户档案
+        testMessage("保存你刚才的操作步骤为文件");
+
+        // 测试 PDF 生成
+        testMessage("生成一份‘七夕约会计划’PDF，包含餐厅预订、活动流程和礼物清单");
+    }
+
+    private void testMessage(String message) {
+        String chatId = UUID.randomUUID().toString();
+        String answer = fuFuApp.doChatWithTools(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+
 }
